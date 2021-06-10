40b3cac5b0d407393fef8872e6442acf62cc4916
nagesh mamillapalli <sidduthecute@gmail.com>
2021-06-10 22:19:28 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using BankAPISample.Service.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAPISample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomer customerservice;
+        public CustomerController (ICustomer customer)
+        {
+            customerservice = customer;
+
+        }
+        [HttpGet]
+        [Route("api/customer/getcustomers")]
+        public IEnumerable<CustomerModel> GetCustomers()
+        {
+            return customerservice.GetCustomers();
+        }
+    }
+}
